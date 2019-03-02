5edc3792e3ead0b72851994de0c480cc9dbf20cf
Arthur Vershyhora <ArturiTunes@gmail.com>
2019-03-02 08:12:34 +0200

    Added alphabet generator and function encrypt

--- Caesar's code/scripts/main.js
+++ Caesar's code/scripts/main.js
@@ -1,7 +1,16 @@
 "use strict";
 let userText = document.querySelector("textarea");
+let alphabetStr = "abcdefghijklmnopqrstuvwxyz";
+let alphabet = alphabetStr.split("");
+function showEncriptedText(text){
+
+}
+function encrypt(text) {
+	let encryptText = text.toUpperCase();
+	console.log(encryptText);
+}
 userText.addEventListener("keyup", ()=>{
 	let text = userText.value;
-	console.log(text);
+	encrypt(text);
 });
 	
