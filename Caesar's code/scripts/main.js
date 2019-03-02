c11a2e39a94f5465039b8a94d0c45a6c5deaae19
Arthur Vershyhora <ArturiTunes@gmail.com>
2019-03-02 08:41:07 +0200

    Finished function encrypt

--- Caesar's code/scripts/main.js
+++ Caesar's code/scripts/main.js
@@ -6,7 +6,16 @@ function showEncriptedText(text){
 
 }
 function encrypt(text) {
-	let encryptText = text.toUpperCase();
+	let encryptText = [];
+	let lettersArray = text.split("");
+	lettersArray.forEach((letter)=>{
+		let index = alphabet.indexOf(letter);
+		let newIndex = index + 3;
+		if (newIndex > alphabet.length) newIndex -= 26;
+		encryptText.push(alphabet[newIndex]);
+	});
+	encryptText = encryptText.join('');
+	encryptText = encryptText.toUpperCase();
 	console.log(encryptText);
 }
 userText.addEventListener("keyup", ()=>{
