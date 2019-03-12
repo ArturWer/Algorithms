c2cd5830bf374509f9cfbd4addd9335f5d640f4c
Arthur Vershyhora <ArturiTunes@gmail.com>
2019-03-12 14:03:10 +0200

    Realised adding space every five symbol

--- Caesar's code/scripts/main.js
+++ Caesar's code/scripts/main.js
@@ -1,22 +1,28 @@
 "use strict";
-let userText = document.querySelector("textarea");
-let alphabetStr = "abcdefghijklmnopqrstuvwxyz";
-let alphabet = alphabetStr.split("");
+let userText = document.querySelector(".userText");
+let alphabetEnStr = "abcdefghijklmnopqrstuvwxyz";
+let alphabet = alphabetEnStr.split("");
 function showEncriptedText(text){
 
 }
 function encrypt(text) {
 	let encryptText = [];
 	let lettersArray = text.split("");
+	let arrayWithSpaces = [];
 	lettersArray.forEach((letter)=>{
 		let index = alphabet.indexOf(letter);
 		let newIndex = index + 3;
 		if (newIndex > alphabet.length) newIndex -= 26;
 		encryptText.push(alphabet[newIndex]);
 	});
-	encryptText = encryptText.join('');
-	encryptText = encryptText.toUpperCase();
-	console.log(encryptText);
+	//added SPACES
+	for (let i = 0; i <= encryptText.length; i++) {
+		if (i%4 === 0) arrayWithSpaces.push(" ");
+		arrayWithSpaces.push(encryptText[i]);
+	}
+	arrayWithSpaces = arrayWithSpaces.join('');
+	arrayWithSpaces = arrayWithSpaces.toUpperCase();
+	console.log(arrayWithSpaces);
 }
 userText.addEventListener("keyup", ()=>{
 	let text = userText.value;
