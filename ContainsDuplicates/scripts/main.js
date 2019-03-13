e7e9767d174661483cd81edac693b5c8cd4d55c6
Arthur Vershyhora <ArturiTunes@gmail.com>
2019-03-13 06:34:10 +0200

    added generation random numbers and showing they

--- ContainsDuplicates/scripts/main.js
+++ ContainsDuplicates/scripts/main.js
@@ -1,2 +1,12 @@
 "use strict"
-	
+let myArray	= [];
+let numbers = document.querySelector(".numbers");
+
+function random(num){
+  return Math.random()*num;
+}
+for (let i = 0; i<=300; i++) {
+	let randomNum = Math.floor(random(400));
+	myArray.push(randomNum);
+}
+numbers.textContent = myArray.join(', ');
