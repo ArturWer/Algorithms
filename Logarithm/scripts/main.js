d94c4c57d6d396ffe1d94329959e4e97070cc387
Arthur Vershyhora <ArturiTunes@gmail.com>
2019-03-16 07:09:02 +0200

    Added addEventListeners

--- Logarithm/scripts/main.js
+++ Logarithm/scripts/main.js
@@ -1,6 +1,18 @@
 "use strict"
 let logarithm = document.querySelector(".logarithm");
-let msg = `Log <sub>b</sub> <span>a</span>`;
+let inputA = document.getElementById('numberA');
+let inputB = document.getElementById('numberB');
+let aValue = document.querySelector(".a");
+let bValue = document.querySelector(".b");
+let cValue = document.querySelector(".c");
 
-//logarithm.textContent = msg;
-logarithm.innerHTML = msg;	
+function showUserInput(e) {
+	if (e.target.id === "numberA") {
+		aValue.textContent = e.target.value;
+	} else if (e.target.id === "numberB") {
+		bValue.textContent = e.target.value;
+	}	
+}
+
+inputA.addEventListener("change",showUserInput);
+inputB.addEventListener("change",showUserInput);
