1feec4bdd5af580c9dd713dc657747f185a50ecb
Arthur Vershyhora <ArturiTunes@gmail.com>
2019-03-28 14:25:11 +0200

    Show result on page

--- makePairs/scripts/main.js
+++ makePairs/scripts/main.js
@@ -1,13 +1,21 @@
 "use strict"
+let main = document.querySelector("main")
 let lettersString = 'abcdefghijklmnopqrstuvwxyz';
 lettersString = lettersString.toUpperCase();
 let lettersArr = lettersString.split('');
 let pairs = [];
 console.log(lettersArr);
 
+let writePairs = ()=>{
+	let para = document.createElement("p");
+	para.textContent = pairs.join(", ");
+	main.appendChild(para);
+}
+
 for (var i = 0; i < lettersArr.length; i++) {
 	for (var j = i+1; j < lettersArr.length; j++) {	
 		pairs.push(`${lettersArr[i]+lettersArr[j]}`);
 	}
 }
-console.log(pairs);
+writePairs();
+
