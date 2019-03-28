04f0002a5cc5a5205af3b33ac51762e72a417508
Arthur Vershyhora <ArturiTunes@gmail.com>
2019-03-28 14:15:36 +0200

    Added forming PAIRS Array

--- makePairs/scripts/main.js
+++ makePairs/scripts/main.js
@@ -2,4 +2,12 @@
 let lettersString = 'abcdefghijklmnopqrstuvwxyz';
 lettersString = lettersString.toUpperCase();
 let lettersArr = lettersString.split('');
+let pairs = [];
 console.log(lettersArr);
+
+for (var i = 0; i < lettersArr.length; i++) {
+	for (var j = i+1; j < lettersArr.length; j++) {	
+		pairs.push(`${lettersArr[i]+lettersArr[j]}`);
+	}
+}
+console.log(pairs);
