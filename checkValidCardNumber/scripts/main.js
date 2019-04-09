3d1ce1ab496c1e176393f4524669eafc672d92c4
Arthur Vershyhora <ArturiTunes@gmail.com>
2019-04-09 18:02:38 +0300

    Added eventListener and some validation

--- checkValidCardNumber/scripts/main.js
+++ checkValidCardNumber/scripts/main.js
@@ -1,3 +1,20 @@
 "use strict";
 let msg = document.querySelector(".msg");
-	
+let cardClass = document.querySelector(".cardClass");
+let cardNum = document.getElementById("cardNum");
+
+function validCard(number) {
+	cardClass.textContent = "";
+	msg.textContent = number;
+	console.log(number[0]);
+	if (number[0] === "4") {
+		cardClass.textContent = "Visa";
+	} else if (number[0] === "5") {
+		if (number[1] === "1" || number[1] === "2" || number[1] === "3" || number[1] === "4" || number[1] === "5") {
+			cardClass.textContent = "MasterCard";
+		}
+	}
+}
+cardNum.addEventListener("input", (e)=>{
+	validCard(e.target.value);
+});
