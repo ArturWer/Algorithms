ab0b8decbd12f980c05e2f3d16d2fbfc657bc1c3
Arthur Vershyhora <ArturiTunes@gmail.com>
2019-04-10 08:57:47 +0300

    Added 1st part of algorithm

--- checkValidCardNumber/scripts/main.js
+++ checkValidCardNumber/scripts/main.js
@@ -4,13 +4,34 @@ let cardClass = document.querySelector(".cardClass");
 let cardNum = document.getElementById("cardNum");
 
 function validCard(number) {
+	let cardNum = number.toString();
+	let evenNumbers = [];
+	let sum = 0;
 	cardClass.textContent = "";
-	msg.textContent = number;
-	console.log(number[0]);
-	if (number[0] === "4") {
+	msg.textContent = cardNum;
+	if (cardNum.length !== 16) return;
+	/* apply check algorithm */
+	else if (cardNum.length === 16) {
+		for (var i = 0; i < cardNum.length; i++) {
+			if(i%2 === 0)
+				evenNumbers.push(cardNum[i]*2);
+		}
+		console.log(evenNumbers);
+		evenNumbers.forEach((num)=>{
+			let nums;
+			if (num<10) {
+				sum+=num;
+			} else {
+				nums = num.toString();
+				sum+=(Number(nums[0])+Number(nums[1]));
+			}
+		});
+		console.log(sum);
+	}
+	if (cardNum[0] === "4") {
 		cardClass.textContent = "Visa";
-	} else if (number[0] === "5") {
-		if (number[1] === "1" || number[1] === "2" || number[1] === "3" || number[1] === "4" || number[1] === "5") {
+	} else if (cardNum[0] === "5") {
+		if (cardNum[1] === "1" || cardNum[1] === "2" || cardNum[1] === "3" || cardNum[1] === "4" || cardNum[1] === "5") {
 			cardClass.textContent = "MasterCard";
 		}
 	}
@@ -18,3 +39,4 @@ function validCard(number) {
 cardNum.addEventListener("input", (e)=>{
 	validCard(e.target.value);
 });
+validCard(cardNum.value);
