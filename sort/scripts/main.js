603f8f7809b5c94faef22588118f0880aac66418
Arthur Vershyhora <ArturiTunes@gmail.com>
2019-04-13 09:21:01 +0300

    Unsorted array

--- sort/scripts/main.js
+++ sort/scripts/main.js
@@ -1,2 +1,13 @@
 "use strict"
-	
+let numsArr = [];
+
+function random(num){
+  return Math.random()*num;
+}	
+
+for (var i = 0; i < 100000; i++) {
+	let num = Math.floor(random(500));
+	numsArr.push(num);
+}
+
+	console.log(`Unsorted array is: ${numsArr}`);
