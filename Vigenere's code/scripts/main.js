231b0883c217411b2cc9d94d81ff454f3d9ef241
Arthur Vershyhora <ArturiTunes@gmail.com>
2019-04-24 14:45:52 +0300

    Added makeAlphabetArray function

--- Vigenere's code/scripts/main.js
+++ Vigenere's code/scripts/main.js
@@ -1,11 +1,30 @@
 "use strict";
 let key = document.querySelector(".key");
+let userText = document.querySelector(".userText");
+let alphabet = "abcdefghijklmnopqrstuvwxyz";
+let alphArr = [];
 
+let cipher = (text)=>{
+	let codeText = "";
+	let keyWord = key.value;
+	for (var i = 0; i < text.length; i++) {
+		console.log(text.charCodeAt(i));
+	}
+}
+
+function makeAlphabetArray (){
+	for (var i = 0; i < alphabet.length; i++) {
+		alphArr.push(alphabet[i]);
+	}
+}
 key.addEventListener("input", e =>{
 	let val = e.target.value;
-	console.log(val);
-	/*val.forEach(char=>{
-		console.log(char);
-	});*/
+	cipher(userText.value);
 });
-	
+
+userText.addEventListener("input", e =>{
+	let val = e.target.value;
+	cipher(val);
+});	
+
+makeAlphabetArray();
